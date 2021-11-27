d2381babfae813c829327cdf2507a2cd349874e1
David Boh <herrboh@gmail.com>
2021-11-27 00:10:29 +0000

    update database connection settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,15 +78,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'dj_user',
-        'NAME': 'django_mysite',
-        'PASSWORD': 'postmalone.',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
