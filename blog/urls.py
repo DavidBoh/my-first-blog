87bd2720482ff2f787d01696c0acad3d35c2fb5e
David Boh <herrboh@gmail.com>
2022-01-20 18:15:13 -0500

    add publish button, delete post button, add security decorators to views.py, add django login form

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from . import views
+from . import views as p
+
 
 urlpatterns = [
-        path('', views.post_list, name='post_list'),
-        path('post/<int:pk>/', views.post_detail, name='post_detail'),
-        path('post/new',views.post_new, name='post_new'),
-        path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
-        
+        path('', p.post_list, name='post_list'),
+        path('post/<int:pk>/', p.post_detail, name='post_detail'),
+        path('post/new',p.post_new, name='post_new'),
+        path('post/<int:pk>/edit/', p.post_edit, name='post_edit'),
+        path('drafts/', p.post_draft_list, name="post_draft_list"),
+        path('post/<pk>/publish/', p.post_publish, name='post_publish'),
+        path('post/<pk>/remove', p.post_remove, name='post_remove'),
 ]        
 
 
